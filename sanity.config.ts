8758e55d55346d94ca3983fef0944cce4d205bc1
Lukyord <ttansiri524@gmail.com>
2022-12-27 16:59:28 +0700

    fixed typo

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,14 +4,14 @@ import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 
 const projectId: string = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset: string = process.env.EXT_PUBLIC_SANITY_DATASET!;
+const dataset: string = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
   basePath: "/studio",
   name: "nextjs13_sanity_blog",
   title: "Nextjs13 Sanity Blog",
-  projectId: "7e0x7tym",
-  dataset: "production",
+  projectId,
+  dataset,
   plugins: [deskTool(), visionTool()],
   schema: {
     types: schemaTypes,
