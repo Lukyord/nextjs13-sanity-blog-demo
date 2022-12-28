cd1ac31bbdc7644e55cbbbb8bca9659dfa57e574
Lukyord <ttansiri524@gmail.com>
2022-12-28 17:47:37 +0700

    added preview suspense and preview mode

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
+import PreviewSuspense from "../../components/PreviewSuspense/PreviewSuspense";
 
-function HomePage() {
-  return <div></div>;
+const query = groq`
+  *[_type=='post'] {
+    ...,
+    author->,
+    categories[]->,
+  } | order(_createAt desc)
+`;
+
+async function HomePage() {
+  if (previewData()) {
+    return (
+      <PreviewSuspense
+        fallback={
+          <div role="status">
+            <p className="text-center text-lg animate-pulse text-[#c27ba0]">
+              Loading Preview Data
+            </p>
+          </div>
+        }
+      >
+        <div></div>
+      </PreviewSuspense>
+    );
+  }
+
+  const posts = await client.fetch(query);
+  console.log(posts);
+
+  return <div>Not in Preview Mode</div>;
 }
 
 export default HomePage;
